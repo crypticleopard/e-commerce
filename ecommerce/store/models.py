76a6c21f26b68cb7c1444d9bff4a844a765fe18d
crypticleopard <tarunsingh3443@gmail.com>
2020-12-01 18:26:16 +0530

    Third commit

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -13,6 +13,34 @@ class Product(models.Model):
     name=models.CharField(max_length=200,null=True)
     price=models.FloatField()
     digital=models.BooleanField(default=False)
+    image=models.ImageField(null=True,blank=True)
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
+    date_ordered=models.DateTimeField(auto_now_add=True)
+    complete=models.BooleanField(default=False)
+    transaction_id=models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return str(self.transaction_id)
+
+class OrderItem(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
+    quantity=models.IntegerField(default=0,null=True,blank=True)
+    date_added=models.DateTimeField(auto_now_add=True)
+    
+class ShipppingAddress(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
+    address=models.CharField(max_length=200,null=True)
+    city=models.CharField(max_length=200,null=True)
+    state=models.CharField(max_length=200,null=True)
+    pincode=models.CharField(max_length=200,null=True)
+    date_added=models.DateTimeField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.address
